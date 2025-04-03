928016c7c6002bfcab16d1738bae0c66c7bf926c
Agustin Osatinsky <agustinosatinsky@hotmail.com>
2025-04-03 13:31:10 +0300

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/solana-test-dapp/",
   define: {
     "process.env": {},
     global: {},
